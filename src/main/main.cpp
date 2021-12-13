0e8da9d01997194a2a8e891d3df8a75cc122a14a
Dr.Zany <sollberger.n@gmail.com>
2021-12-13 13:46:26 +0100

    init docker for Firmware

---
+++ src/main/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author noel
+ * @brief
+ * @version 0.1
+ * @date 2021-12-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "pico/stdlib.h"
+#include "stdio.h"
+
+int main(void)
+{
+
+  return 0;
+}
